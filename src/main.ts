6c6d86e593f66a6a8ae2719e772706908107e3ab
wangzhitao <1542344316@qq.com>
2023-08-24 21:04:11 +0800

    feat:new function

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { createApp } from 'vue'
+import App from '@/App.vue'
+//引入模板的全局的样式
+import '@/styles/index.scss'
+//引入element-plus插件与样式
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//svg插件需要配置代码
+import 'virtual:svg-icons-register'
+//引入自定义插件对象:注册整个项目全局组件
+import globalComponent from '@/components'
+//配置element-plus国际化
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+//暗黑模式需要的样式
+import 'element-plus/theme-chalk/dark/css-vars.css'
+//引入路由
+import router from './router'
+import '@/styles/style.css'
+//获取应用实例对象
+const app = createApp(App)
+//安装element-plus插件
+app.use(ElementPlus, {
+  locale: zhCn, //element-plus国际化配置
+})
+//安装自定义插件
+app.use(globalComponent)
+//注册模板路由
+app.use(router)
+//将应用挂载到挂载点上
+app.mount('#app')
